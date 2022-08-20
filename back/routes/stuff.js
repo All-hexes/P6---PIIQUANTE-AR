efbe3fcfc9d6f919718ec708df00c0a281ff47fb
Alexandre Roblain <alexrob260@gmail.com>
2022-08-20 19:11:41 +0200

    stuff.js & thing.js

---
+++ back/routes/stuff.js
@@ -0,0 +1,23 @@
+// on importe toujours express pour récupérer ses méthodes
+const express = require("express");
+
+// on utlise la méthode "routeur" de "express"
+const router = express.Router();
+
+const auth = require("../middleware/auth");
+
+const multer = require("../middleware/multer-config");
+
+const stuffCtrl = require("../controllers/stuff");
+
+router.post("/", auth, multer, stuffCtrl.createThing);
+
+router.put("/:id", auth, multer, stuffCtrl.modifyThing);
+
+router.delete("/:id", auth, stuffCtrl.deleteThing);
+
+router.get("/:id", auth, stuffCtrl.getOneThing);
+
+router.get("/", auth, stuffCtrl.getAllThings);
+
+module.exports = router;
